e2f5bf9526b343a1049d3fbe55783e97350b5c71
Maria <mariafrancia88@gmail.com>
2017-10-01 21:17:15 +0200

    Task 9-12

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,14 +1,7 @@
 <div class="col-6 col-md-3 sidebar-offcanvas" id="sidebar">
           <div class="list-group">
-            <a href="#" class="list-group-item active">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
+        @for($i = count($movies)-1; $i>count($movies)-6; $i--)
+            <a href="/movies/{{$movies[$i]}}" class="list-group-item">{{$movies[$i]->title}}</a>
+        @endfor
           </div>
         </div><!--/span-->
