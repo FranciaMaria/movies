105826ca8daa2cd9252f0688c2e17d8e61dcef4d
Maria <mariafrancia88@gmail.com>
2017-10-02 10:06:31 +0200

    modified task 12

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,13 @@
 <div class="col-6 col-md-3 sidebar-offcanvas" id="sidebar">
           <div class="list-group">
-        @for($i = count($movies)-1; $i>count($movies)-6; $i--)
-            <a href="/movies/{{$movies[$i]}}" class="list-group-item">{{$movies[$i]->title}}</a>
-        @endfor
+        @if(count($movies) >= 5)
+        	@for($i = count($movies)-1; $i>count($movies)-6; $i--)
+            	<a href="/movies/{{$movies[$i]}}" class="list-group-item">{{$movies[$i]->title}}</a>
+        	@endfor
+        @else
+        	@for($i = count($movies)-1; $i>0; $i--)
+            	<a href="/movies/{{$movies[$i]}}" class="list-group-item">{{$movies[$i]->title}}</a>
+        	@endfor
+        @endif
           </div>
         </div><!--/span-->
